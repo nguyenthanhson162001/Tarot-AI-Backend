d7446c2dc1533b6eb7a948f5b788c8dd153f0ab1
sown <nguyenthanhson162001@gmail.com>
2024-05-02 11:15:05 +0700

    Feat: Add docker compose app and mongodb

--- src/@bootstrap/server.ts
+++ src/@bootstrap/server.ts
@@ -1,84 +1,80 @@
-import { NestFactory } from "@nestjs/core";
-import { NestExpressApplication } from "@nestjs/platform-express";
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import rateLimit from 'express-rate-limit'
-import helmet from "helmet";
-import * as cookieParser from 'cookie-parser';
-import * as compression from 'compression';
+import helmet from 'helmet'
+import * as cookieParser from 'cookie-parser'
+import * as compression from 'compression'
 import * as morgan from 'morgan'
-import {
-    DocumentBuilder,
-    SwaggerModule,
-} from '@nestjs/swagger';
-
-import { RootModule } from "../modules/root.module";
-import { join } from "path";
-import { ENVConfig } from "src/infrastructure/configuration";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { RootModule } from '../modules/root.module'
+import { join } from 'path'
+import { ENVConfig } from 'src/infrastructure/configuration'
+import { Logger } from '@nestjs/common'
 
 export default class Server {
-    private app: NestExpressApplication;
+  private app: NestExpressApplication
+  private logger: Logger
+  private constructor(app: NestExpressApplication) {
+    this.app = app
+    this.logger = new Logger(Server.name)
+  }
 
+  private async setupMiddleware() {
+    this.app.setGlobalPrefix(ENVConfig.GLOBAL_PREFIX)
 
-    private constructor(app: NestExpressApplication) {
-        this.app = app;
+    this.app.enableCors({ origin: ENVConfig.CORS_ORIGIN })
+
+    this.app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }))
+
+    this.app.use(helmet())
+
+    this.app.use(cookieParser())
+
+    this.app.use(compression())
+
+    this.app.use(morgan('dev'))
+
+    this.app.useStaticAssets(join(__dirname, 'static'))
+
+    this.setupSwagger()
+  }
+
+  private setupSwagger() {
+    if (ENVConfig.IS_SWAGGER_ENABLED) {
+      const options = new DocumentBuilder()
+        .setTitle(ENVConfig.SWAGGER_TITLE)
+        .setDescription(ENVConfig.SWAGGER_DESCRIPTION)
+        .setVersion(ENVConfig.APP_VERSION)
+
+      ENVConfig.SWAGGER_SERVERS.forEach((server) => options.addServer(server))
+
+      const document = SwaggerModule.createDocument(this.app, options.build())
+
+      SwaggerModule.setup(ENVConfig.SWAGGER_PREFIX, this.app, document)
     }
+  }
 
-    private async setupMiddleware() {
-        this.app.setGlobalPrefix(ENVConfig.GLOBAL_PREFIX);
+  public static async initial() {
+    const app = await NestFactory.create<NestExpressApplication>(RootModule)
 
-        this.app.enableCors({ origin: ENVConfig.CORS_ORIGIN });
+    const server = new Server(app)
 
-        this.app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+    await server.setupMiddleware()
 
-        this.app.use(helmet());
+    return server
+  }
 
-        this.app.use(cookieParser());
+  public async start() {
+    this.app.listen(ENVConfig.PORT, () => {
+      console.log('\n------------------------------------------------------------------\n')
+      console.log(`Server running on port ${ENVConfig.BASE_URL}`)
 
-        this.app.use(compression());
+      if (ENVConfig.IS_SWAGGER_ENABLED) {
+        console.log(`Swagger running on ${ENVConfig.BASE_URL}/${ENVConfig.SWAGGER_PREFIX}`)
+      }
 
-        this.app.use(morgan('combined'));
-
-        this.app.useStaticAssets(join(__dirname, 'static'));
-
-        this.setupSwagger();
-    }
-
-    private setupSwagger() {
-        if (ENVConfig.IS_SWAGGER_ENABLED) {
-            const options = new DocumentBuilder()
-                .setTitle(ENVConfig.SWAGGER_TITLE)
-                .setDescription(ENVConfig.SWAGGER_DESCRIPTION)
-                .setVersion(ENVConfig.APP_VERSION)
-
-            ENVConfig.SWAGGER_SERVERS.forEach(server => options.addServer(server))
-
-            const document = SwaggerModule.createDocument(this.app, options.build());
-
-            SwaggerModule.setup(ENVConfig.SWAGGER_PREFIX, this.app, document);
-
-        }
-    }
-
-    public static async initial() {
-        const app = await NestFactory.create<NestExpressApplication>(RootModule);
-
-        const server = new Server(app);
-
-        await server.setupMiddleware();
-
-        return server;
-    }
-
-    public async start() {
-        this.app.listen(ENVConfig.PORT, () => {
-            console.log("\n------------------------------------------------------------------\n")
-            console.log(`Server running on port ${ENVConfig.BASE_URL}`);
-
-            if (ENVConfig.IS_SWAGGER_ENABLED) {
-                console.log(`Swagger running on ${ENVConfig.BASE_URL}/${ENVConfig.SWAGGER_PREFIX}`);
-                require('child_process').exec(`open ${ENVConfig.BASE_URL}/${ENVConfig.SWAGGER_PREFIX}`);
-            }
-
-            console.log("\n------------------------------------------------------------------\n")
-        });
-    }
-}  
+      console.log('\n------------------------------------------------------------------\n')
+    })
+    this.logger.error(`Server is listening at port:`)
+  }
+}
--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { API_TAGS } from 'src/infrastructure/swagger';
-import { appPaths } from './app.constants';
-
+import { Controller, Get, Logger } from '@nestjs/common'
+import { AppService } from './app.service'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
+import { API_TAGS } from 'src/infrastructure/swagger'
+import { appPaths } from './app.constants'
 
 @Controller(appPaths.prefix)
 @ApiTags(API_TAGS.HEALTH)
 export class AppController {
-
-  constructor(private readonly appService: AppService) { }
+  private readonly logger = new Logger(AppController.name)
+  constructor(private readonly appService: AppService) {}
 
   @Get(appPaths.checkHealth.path)
   @ApiOperation(appPaths.checkHealth.operation)
   checkHealth(): string {
-    return this.appService.checkHealth();
+    this.logger.log('Checking health')
+    return this.appService.checkHealth()
   }
 }
